b17de26db2ed9530522bf172462de28b28e591fa
Yinglin Luan <synmyth@gmail.com>
2012-02-28 18:58:25 +0800

    use deque instead of vector in stack

--- stack.h
+++ stack.h
@@ -1,12 +1,9 @@
 #ifndef _STACK_H_
 #define _STACK_H_
-#include "vector.h"
+#include "deque.h"
 
 struct stack {
-	struct vector v;
-	size_t elem_size;
-	void (*copy)(void *dest, void *src);
-	void (*free)(void *element);
+	struct deque d;
 };
 
 inline void stack_init(struct stack *s, size_t elem_size,
@@ -23,61 +20,49 @@ inline void stack_init(struct stack *s, size_t elem_size,
 		void (*copy_func)(void *, void *), void (*free_func)(void *))
 {
 	assert(s);
-
-	vector_init(&s->v, elem_size, copy_func, free_func);
-	s->elem_size = elem_size;
-	s->copy = copy_func;
-	s->free = free_func;
+	deque_init(&s->d, elem_size, copy_func, free_func);
 }
 
 /* destroy the stack */
 inline void stack_destroy(struct stack *s)
 {
 	assert(s);
-	vector_destroy(&s->v);
+	deque_destroy(&s->d);
 }
 
 /* checks whether the container is empty */
 inline int stack_empty(struct stack *s)
 {
 	assert(s);
-	return vector_empty(&s->v);
+	return deque_empty(&s->d);
 }
 
 /* returns the number of elements */
 inline size_t stack_size(struct stack *s)
 {
 	assert(s);
-	return vector_size(&s->v);
+	return deque_size(&s->d);
 }
 
 /* inserts element at the top */
 inline void stack_push(struct stack *s, void *element)
 {
 	assert(s && element);
-	vector_append(&s->v, element);
+	deque_push_back(&s->d, element);
 }
 
 /* removes the top element */
 inline void stack_pop(struct stack *s, void *element)
 {
 	assert(s);
-
-	void *tmp = stack_top(s);
-	if (s->copy != NULL) {
-		s->copy(element, tmp);
-	} else {
-		__default_copy_func(element, tmp, s->elem_size);
-	}
-
-	vector_delete(&s->v, vector_size(&s->v) - 1);
+	deque_pop_back(&s->d, element);
 }
 
 /* access the top element */
 inline void* stack_top(struct stack *s)
 {
 	assert(s);
-	return vector_back(&s->v);
+	return deque_back(&s->d);
 }
 
 #endif
