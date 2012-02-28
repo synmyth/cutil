1b01a1c7d914cab92f2bfcd5622c590bb3e76817
Yinglin Luan <synmyth@gmail.com>
2012-02-28 18:40:10 +0800

    add comments in stack.h

--- stack.h
+++ stack.h
@@ -18,6 +18,7 @@ inline void stack_push(struct stack *s, void *element);
 inline void stack_pop(struct stack *s, void* element);
 inline void* stack_top(struct stack *s);
 
+/* initialize the stack */
 inline void stack_init(struct stack *s, size_t elem_size,
 		void (*copy_func)(void *, void *), void (*free_func)(void *))
 {
@@ -29,30 +30,35 @@ inline void stack_init(struct stack *s, size_t elem_size,
 	s->free = free_func;
 }
 
+/* destroy the stack */
 inline void stack_destroy(struct stack *s)
 {
 	assert(s);
 	vector_destroy(&s->v);
 }
 
+/* checks whether the container is empty */
 inline int stack_empty(struct stack *s)
 {
 	assert(s);
 	return vector_empty(&s->v);
 }
 
+/* returns the number of elements */
 inline size_t stack_size(struct stack *s)
 {
 	assert(s);
 	return vector_size(&s->v);
 }
 
+/* inserts element at the top */
 inline void stack_push(struct stack *s, void *element)
 {
 	assert(s && element);
 	vector_append(&s->v, element);
 }
 
+/* removes the top element */
 inline void stack_pop(struct stack *s, void *element)
 {
 	assert(s);
@@ -67,6 +73,7 @@ inline void stack_pop(struct stack *s, void *element)
 	vector_delete(&s->v, vector_size(&s->v) - 1);
 }
 
+/* access the top element */
 inline void* stack_top(struct stack *s)
 {
 	assert(s);
