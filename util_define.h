5d95b097b1e5e4e8a2f91cfb846a6559ed79b5e6
Yinglin Luan <synmyth@gmail.com>
2012-02-29 13:11:29 +0800

    add macro CONTAINER_COPY in util_define.h

--- util_define.h
+++ util_define.h
@@ -1,7 +1,17 @@
 #ifndef _UTIL_DEFINE_H_
 #define _UTIL_DEFINE_H_
+#include <stdlib.h>
+#include <string.h>
 
 #define DEFAULT_CONTAINER_CAPACITY	32
+#define CONTAINER_COPY(dest, src, container)				\
+do {									\
+	if ((container)->copy != NULL) {				\
+		(container)->copy(dest, src);				\
+	} else {							\
+		__default_copy_func(dest, src, (container)->elem_size);	\
+	}								\
+} while (0)
 
 inline void __default_copy_func(void *dest, void *src, size_t elem_size)
 {
