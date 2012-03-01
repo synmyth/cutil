98d8e71abba3d7ed3c0efa85f006082a75246cae
Yinglin Luan <synmyth@gmail.com>
2012-03-01 18:50:27 +0800

    remove parameter element in function list_pop_*

--- list.h
+++ list.h
@@ -28,9 +28,9 @@ inline void* list_front(struct list *l);
 inline void* list_back(struct list *l);
 void list_clear(struct list *l);
 inline void list_push_back(struct list *l, void *element);
-inline void list_pop_back(struct list *l, void *element);
+inline void list_pop_back(struct list *l);
 inline void list_push_front(struct list *l, void *element);
-inline void list_pop_front(struct list *l, void *element);
+inline void list_pop_front(struct list *l);
 void list_reverse(struct list *l);
 inline struct list_node* __alloc_new_list_node(struct list *l, void *element);
 inline void __free_list_node(struct list *l, struct list_node *n);
@@ -129,16 +129,13 @@ inline void list_push_back(struct list *l, void *element)
 }
 
 /* removes the last element */
-inline void list_pop_back(struct list *l, void *element)
+inline void list_pop_back(struct list *l)
 {
-	assert(l && element && !list_empty(l));
+	assert(l && !list_empty(l));
 
 	struct list_node *head = &l->head;
 	struct list_node *last = head->prev;
 	struct list_node *node_before_last = last->prev;
-
-	/* copy element */
-	CONTAINER_COPY(element, last->data, l);
 
 	/* remove last node */
 	head->prev = node_before_last;
@@ -168,16 +165,13 @@ inline void list_push_front(struct list *l, void *element)
 }
 
 /* removes the first element */
-inline void list_pop_front(struct list *l, void *element)
+inline void list_pop_front(struct list *l)
 {
-	assert(l && element && !list_empty(l));
+	assert(l && !list_empty(l));
 
 	struct list_node *head = &l->head;
 	struct list_node *first = head->next;
 	struct list_node *node_after_first = first->next;
-
-	/* copy element */
-	CONTAINER_COPY(element, first->data, l);
 
 	/* remove last node */
 	head->next = node_after_first;
