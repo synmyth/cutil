d310fd9d63f4cdb9deeeb4df6c09ce152bf9a0ee
Yinglin Luan <synmyth@gmail.com>
2012-03-03 18:54:09 +0800

    add header file for iterator

---
+++ iterator.h
@@ -0,0 +1,26 @@
+#ifndef _ITERATOR_H_
+#define _ITERATOR_H_
+
+struct iterator {
+	void *ptr;
+	void *data;
+	size_t i;
+	size_t size;
+};
+
+#define UTIL_FOREACH(it, c)	\
+	if ((c))	\
+		for ((c)->iter_head(&(it), (c));	\
+				(it).ptr;	\
+				(c)->iter_next(&(it), (c)))
+
+#define UTIL_FOREACH_REVERSE(it, c)	\
+	if ((c))	\
+		for ((c)->iter_tail(&(it), (c));	\
+				(it).ptr;	\
+				(c)->iter_prev(&(it), (c)))
+
+#define util_foreach		UTIL_FOREACH
+#define util_foreach_reverse	UTIL_FOREACH_REVERSE
+
+#endif
