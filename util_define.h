a0a2a41f1672d61236ffef0d13e32e4375599e6d
Yinglin Luan <synmyth@gmail.com>
2012-03-04 17:15:26 +0800

    simplify macro CONTAINER_COPY

--- util_define.h
+++ util_define.h
@@ -4,18 +4,7 @@
 #include <string.h>
 
 #define DEFAULT_CONTAINER_CAPACITY	32
-#define CONTAINER_COPY(dest, src, container)				\
-do {									\
-	if ((container)->copy != NULL) {				\
-		(container)->copy(dest, src);				\
-	} else {							\
-		__default_copy_func(dest, src, (container)->elem_size);	\
-	}								\
-} while (0)
-
-inline void __default_copy_func(void *dest, void *src, size_t elem_size)
-{
-	memcpy(dest, src, elem_size);
-}
+#define CONTAINER_COPY(dest, src, c)	((c)->copy) ?	\
+	(c)->copy(dest, src) : memcpy(dest, src, (c)->elem_size)
 
 #endif
